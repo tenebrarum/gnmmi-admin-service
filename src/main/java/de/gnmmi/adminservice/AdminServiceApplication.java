16093371ac818ddb41db87fb5c599b6fec0a8398
Chris <christiansrbn@gmail.com>
2018-09-19 22:14:46 +0200

    Initialize SpringBootApplication

---
+++ src/main/java/de/gnmmi/adminservice/AdminServiceApplication.java
@@ -0,0 +1,12 @@
+package de.gnmmi.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServiceApplication.class, args);
+	}
+}
